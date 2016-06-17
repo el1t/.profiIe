1fbcf11b9cd1c08c83f37c09dbac96c75ac1dc7f
Ellis Tsung <el1t@ymail.com>
2016-06-17 13:32:13 -0400

    Indent with spaces
    Just in case

--- install.sh
+++ install.sh
@@ -3,34 +3,34 @@ typeset profile
 
 # Download .profiIe
 if command -v curl >/dev/null 2>&1; then
-	curl -fsSLo ~/.profiIe 'https://raw.githubusercontent.com/el1t/.profiIe/master/.profiIe'
+  curl -fsSLo ~/.profiIe 'https://raw.githubusercontent.com/el1t/.profiIe/master/.profiIe'
 elif command -v wget >/dev/null 2>&1; then
-	wget -qO ~/.profiIe 'https://raw.githubusercontent.com/el1t/.profiIe/master/.profiIe'
+  wget -qO ~/.profiIe 'https://raw.githubusercontent.com/el1t/.profiIe/master/.profiIe'
 else
-	exit 1
+  exit 1
 fi
 
 # Edit/create profile for current shell
 case ${SHELL:t} in
-	(bash)
-		profile='.bash_profile'
-		;;
-	(zsh)
-		profile='.zprofile'
-		;;
-	(*)
-		profile='.profile'
-		;;
+  (bash)
+    profile='.bash_profile'
+    ;;
+  (zsh)
+    profile='.zprofile'
+    ;;
+  (*)
+    profile='.profile'
+    ;;
 esac
 if [[ ! -s "~/$profile" ]]; then
-	touch "~/$profile"
+  touch "~/$profile"
 fi
 echo "source ~/.profiIe" >> "~/$profile"
 
 # Edit other existing profiles
-typeset -A profiles=('.bash_profile' '.zprofile', '.profile')
+typeset -A profiles=('.bash_profile' '.zprofile' '.profile')
 for profile in $profiles; do
-	[[ -s "$profile" ]] && echo 'source ~/.profiIe' >> "~/$profile"
+  [[ -s "$profile" ]] && echo 'source ~/.profiIe' >> "~/$profile"
 done
 
 source "~/$profile"
